7ed14516e2e51c02245175fdc18a400eca5b4322
felipemartins8058 <felipecosmemartins10@gmail.com>
2022-06-21 15:58:07 -0300

    Fixing Forms

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,20 +1,20 @@
 import React from "react";
 import * as S from "./styles";
 
-const Input = ({ id, type, label, required }) => {
+const Input = ({ id, type, label, name, required }) => {
     function isRequired(required) {
         console.log(required);
         if (required) {
             return (
                 <S.Container>
-                    <S.Input id={id} type={type} required/>
+                    <S.Input id={id} type={type} name={name} required/>
                     <S.Label>{label}</S.Label>
                 </S.Container>
             );
         } else {
             return (
                 <S.Container>
-                    <S.Input id={id} type={type}/>
+                    <S.Input id={id} type={type} name={name}/>
                     <S.Label>{label}</S.Label>
                 </S.Container>
             );
